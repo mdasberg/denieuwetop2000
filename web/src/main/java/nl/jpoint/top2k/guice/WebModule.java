6eacaa8bdc3dbc5f829e1064dd47b047a445ac84
Mischa Dasberg <mischa@dasberg.nl>
2012-01-26 14:31:47 +0100

    added dependency injection and rest resource for version

---
+++ web/src/main/java/nl/jpoint/top2k/guice/WebModule.java
@@ -0,0 +1,16 @@
+package nl.jpoint.top2k.guice;
+
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import nl.jpoint.top2k.rest.VersionResource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** Web Module */
+public class WebModule extends ServletModule {
+    protected void configureServlets() {
+        bind(VersionResource.class);
+        serve("/rest/*").with(GuiceContainer.class, new HashMap<String, String>());
+    }
+}
---
+++ web/src/main/java/nl/jpoint/top2k/rest/VersionResource.java
@@ -0,0 +1,24 @@
+package nl.jpoint.top2k.rest;
+
+import org.apache.commons.configuration.Configuration;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/** Clieop03 resource. */
+@Singleton
+@Path("/version")
+public class VersionResource {
+    @Inject
+    private Configuration configuration;
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})
+    public String version() {
+        return configuration.getString("version");
+    }
+}
