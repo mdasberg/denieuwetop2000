104416d7dd7318b49ce8dc42e98285a483489b31
Ron Thijssen <ron@MacBook-Pro-van-Ron-Thijssen.local>
2012-01-26 16:25:18 +0100

    first simple Scorer :)

--- domain/src/main/java/nl/jpoint/top2k/domain/Track.java
+++ domain/src/main/java/nl/jpoint/top2k/domain/Track.java
@@ -5,4 +5,15 @@ package nl.jpoint.top2k.domain;
  * tracklist. Every track has a title (see the guidelines for titles) and is credited to one or more artists.
  */
 public class Track {
+
+    private double score;
+
+    public void setScore(double score) {
+        this.score = score;
+
+    }
+
+    public double getScore() {
+        return score;
+    }
 }
