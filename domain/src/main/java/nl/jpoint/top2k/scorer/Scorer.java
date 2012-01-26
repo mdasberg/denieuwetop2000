549d131b4930b5249a2cabb543421082f01d117c
Ron Thijssen <ron@MacBook-Pro-van-Ron-Thijssen.local>
2012-01-26 18:06:25 +0100

    Scorer with Elo rating

--- domain/src/main/java/nl/jpoint/top2k/scorer/Scorer.java
+++ domain/src/main/java/nl/jpoint/top2k/scorer/Scorer.java
@@ -1,6 +1,10 @@
 package nl.jpoint.top2k.scorer;
 
+import com.google.common.collect.Maps;
 import nl.jpoint.top2k.domain.Track;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * User: ron
@@ -16,7 +20,7 @@ public class Scorer {
         this.trackA = trackA;
         this.trackB = trackB;
 
-        
+
     }
 
     public void aWins() {
@@ -32,8 +36,10 @@ public class Scorer {
         double winningScore = winningTrack.getScore();
         double losingScore = losingTrack.getScore();
 
-        winningScore += 1 * (1 - 0.5);
-        losingScore += 1 * (0 - 0.5);
+        EloRating rating = EloRating.getRating(winningScore - losingScore);
+
+        winningScore += 1 * (1 - rating.getLeftScore());
+        losingScore += 1 * (0 - rating.getRightScore());
 
         winningTrack.setScore(winningScore);
         losingTrack.setScore(losingScore);
@@ -41,7 +47,72 @@ public class Scorer {
 
 
     public void noVote() {
+        double winningScore = trackA.getScore();
+        double losingScore = trackB.getScore();
+
+        EloRating rating = EloRating.getRating(winningScore - losingScore);
+
+        winningScore += 1 * (0.5d - rating.getLeftScore());
+        losingScore += 1 * (0.5d - rating.getRightScore());
+
+        trackA.setScore(winningScore);
+        trackB.setScore(losingScore);
+    }
+
+    private int getELO() {
 
 
+        return 0;
+
+    }
+
+    private static class EloRating {
+
+        private double leftScore;
+        private double rightScore;
+
+        private EloRating(double leftScore) {
+            this.leftScore = leftScore;
+            this.rightScore = 1d - leftScore;
+        }
+
+        public double getLeftScore() {
+            return leftScore;
+        }
+
+        public double getRightScore() {
+            return rightScore;
+        }
+
+        private static Map<Integer, EloRating> elos = init();
+
+        private static Map<Integer, EloRating> init() {
+            Map<Integer, EloRating> ratingMap = Maps.newLinkedHashMap();
+            ratingMap.put(25, new EloRating(0.5));
+            ratingMap.put(50, new EloRating(0.53));
+            ratingMap.put(100, new EloRating(0.64));
+            ratingMap.put(150, new EloRating(0.70));
+            ratingMap.put(200, new EloRating(0.76));
+            ratingMap.put(250, new EloRating(0.81));
+            ratingMap.put(350, new EloRating(0.89));
+            ratingMap.put(400, new EloRating(0.92));
+            ratingMap.put(450, new EloRating(0.94));
+            ratingMap.put(500, new EloRating(0.96));
+            ratingMap.put(735, new EloRating(0.99));
+
+            return ratingMap;
+        }
+
+        public static EloRating getRating(double number) {
+            Set<Map.Entry<Integer, EloRating>> entries = elos.entrySet();
+
+            for (Map.Entry<Integer, EloRating> it : entries) {
+                if (number <= it.getKey()) {
+                    return it.getValue();
+                }
+            }
+
+            return new EloRating(1.0d);
+        }
     }
 }
